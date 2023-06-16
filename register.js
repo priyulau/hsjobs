da554c94a7425a9fff4baa1d49a25fa3f6d105d6
Priscilla Lau <pyu@wellesley.edu>
2023-06-16 11:23:08 -0400

    started registration page

--- register.js
+++ register.js
@@ -7,52 +7,68 @@ const password2 = document.getElementById("cpassword");
 const formEl = document.getElementById("form");
 const openMessage = document.querySelector(`#opening-msg`);
 const closeMessage = document.querySelector(`#closing-msg`);
+const resetBtn = document.querySelector(`#reset`);
 
 const validateUserInput = (userInput, fieldName = `Input`) => {
-    if (userInput === "") {
-      showMessage(
-        `Your registration is incomplete. Please complete ${fieldName}.`
-      );
-      return false;
-    }
-    return true;
-  };
+  if (userInput === "") {
+    showMessage(
+      `Your registration is incomplete. Please complete ${fieldName}.`
+    );
+    return false;
+  }
+  return true;
+};
 
-  const validatePassword = (pw1, pw2) => {
-    if (pw1 !== pw2) {
-      showMessage(`Your passwords do not match!`);
-      return false;
-    }
-    return true;
-  };
+const validatePassword = (pw1, pw2) => {
+  if (pw1 !== pw2) {
+    showMessage(`Your passwords do not match!`);
+    return false;
+  }
+  return true;
+};
 
-  formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (
-      validateUserInput(firstName.value, `First Name`) &&
-      validateUserInput(lastName.value, `Last Name`) &&
-      validateUserInput(email.value, `Email Address`) &&
-      validateUserInput(password1.value, `Password`) &&
-      validatePassword(password1.value, password2.value)
-    ) {
-      registerUser();
-    }
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNameVal = firstName.value;
+  const lastNameVal = lastName.value;
+  const emailVal = email.value;
+  const password1Val = password1.value;
+  const password2Val = password2.value;
+
+  if (
+    validateUserInput(firstNameVal, `First Name`) &&
+    validateUserInput(lastNameVal, `Last Name`) &&
+    validateUserInput(emailVal, `Email Address`) &&
+    validateUserInput(password1Val, `Password`) &&
+    validatePassword(password1Val, password2Val)
+  ) {
+    console.log(`validated`); //works
+    registerUser({
+      firstNameVal,
+      lastNameVal,
+      emailVal,
+      password1Val,
+      password2Val,
+    });
+    //add condiition here because we only want to log-in the user once they have been registered
+    if (registerUser()) console.log(emailVal, password1Val);
+  }
 });
 
 function registerUser() {
-    showMessage(`You are now registered!`);
-    showMessage(`Let's go log-in!`, closeMessage);
-  }
+  showMessage(`You are now registered!`);
+  showMessage(`Let's go log-in!`, closeMessage);
+}
 
-  function showMessage(msg, msgDom = openMessage) {
-    msgDom.innerHTML = `<p>${msg}</p>`;
-  }
+function showMessage(msg, msgDom = openMessage) {
+  msgDom.innerHTML = `<p>${msg}</p>`;
+}
 
-  function clearMessage(msg, msgDom = openMessage) {
-    msgDom.innerHTML = ``;
-  }
+function clearMessage(msg, msgDom = openMessage) {
+  msgDom.innerHTML = ``;
+}
 
-  //FUNCTION -logs user in
+//FUNCTION -logs user in
 function loginUser(email, password) {
   console.log(email, password);
   showMessage(
@@ -62,3 +78,14 @@ function loginUser(email, password) {
     window.location = `https://google.com`;
   }, 2000);
 }
+
+//FUNCTION resets form
+resetBtn.addEventListener("click", (event) => {
+  firstName.value = "";
+  lastName.value = "";
+  email.value = "";
+  password1.value = "";
+  password2.value = "";
+  openMessage.innerHTML = "";
+  closeMessage.innerHTML = "";
+});
